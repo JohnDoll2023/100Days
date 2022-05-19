f8f6339e5b8461193d1bad82f1f071340cb7f6a2
dolljm <dolljm@miamioh.edu>
2022-05-19 15:58:58 -0700

    almost done with project7
    facing issues with the tab bar controller

---
+++ Project7/Project7/Petition.swift
@@ -0,0 +1,14 @@
+//
+//  Petition.swift
+//  Project7
+//
+//  Created by John Doll on 5/19/22.
+//
+
+import Foundation
+
+struct Petition: Codable {
+    var title: String
+    var body: String
+    var signatureCount: Int
+}
---
+++ Project7/Project7/Petitions.swift
@@ -0,0 +1,12 @@
+//
+//  Petitions.swift
+//  Project7
+//
+//  Created by John Doll on 5/19/22.
+//
+
+import Foundation
+
+struct Petitions: Codable {
+    var results: [Petition]
+}
